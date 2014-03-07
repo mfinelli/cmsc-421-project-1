289024dbb6fb194bd058b151be1bbc1404302568
Mario Finelli <mfinelli@umd.edu>
2014-03-07 18:44:52 -0500

    Initial logic for problem 1.

--- finelli_project1.clj
+++ finelli_project1.clj
@@ -2,11 +2,22 @@
 ;; CMSC 421 - Section 0201
 ;; Project 1 (Due 13 March 2014)
 
+;; for each queen in the partial solution check if it is at the same row as
+;; the new queen, then check if it conflicts vertically with the new queen
+;; (number of columns to the end +/- current rank)
 (defn qextends?
   "Returns true if a queen at rank extends partial-sol."
   [partial-sol rank]
-    nil)
- 
+    (for [queen partial-sol
+          pos (range 1 (count partial-sol)) :while (< pos (count partial-sol))]
+      (if (= queen rank)
+        false
+        (if (= (+ queen (- (count partial-sol) pos)) rank)
+          false
+          (if (= (- queen (- (count partial-sol) pos)) rank)
+            false
+            true)))))
+
 (defn qextend
   "Given a vector *partial-sol-list* of all partial solutions of length k,
   returns a vector of all partial solutions of length k + 1."
