95b3b1081c8703accef9c61c3421a8b53e8d53e4
Mario Finelli <mfinelli@umd.edu>
2014-03-08 14:55:34 -0500

    Update public tests.

--- public_test.clj
+++ public_test.clj
@@ -1,12 +1,44 @@
-(load-file "lastname_project1.clj")
+(load-file "finelli_project1.clj")
 (use 'clojure.test)
 
-(deftest sol-count-test
+;; problem 1: qextends?
+(deftest qextends?-test1
+  (is (qextends? [1 3] 5)))
+
+(deftest qextends?-test2
+  (is (qextends? [1 4] 2)))
+
+(deftest qextends?-test3
+  (is (not (qextends? [1 3] 2))))
+
+;; problem 2: qextend
+(deftest qextend-test1
+  (is (= [[3 1]] (qextend 4 [[3]]))))
+
+(deftest qextend-test2
+  (is (= [[1 4 2] [1 3 5]] (qextend 5 [[1 3] [1 4]]))))
+
+(deftest qextend-test3
+  (is (= [] (qextend 4 [[1 3]]))))
+
+;; problem 3: sol-count
+(deftest sol-count-test1
+  (is (= 1 (sol-count 1))))
+
+(deftest sol-count-test2
+  (is (= 0 (sol-count 2))))
+
+(deftest sol-count-test3
+  (is (= 0 (sol-count 3))))
+
+(deftest sol-count-test4
   (is (= 10 (sol-count 5))))
 
-(deftest extends?-test
-  (is (qextends? [1 3] 5)))
-  
-#! Write your own tests here...
-  
+(deftest sol-count-test5
+  (is (= 92 (sol-count 8))))
+
+;; problem 4: sol-density
+(deftest sol-density-test1
+  (is (= 0.0032 (sol-density 5))))
+ 
 (run-tests)
